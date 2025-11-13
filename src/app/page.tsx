6b3a6303c91ba0b3fb5b5b535adc6908a4eae07d
thorsanchez <ths281@hi.is>
2025-11-13 17:41:51 +0000

    bæta við post takka

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ import { CiBookmark } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
 import Link from "next/link";
 import { LuCross } from "react-icons/lu";
-
+import { PiHouse } from "react-icons/pi";
 
 //https://react-icons.github.io/react-icons/
 const navigation_items = [
   {
-    title:"x",
-    icon:LuCross,
+    title: "x",
+    icon: LuCross,
   },
   {
     title: "Home",
-    icon: MdHomeFilled,
+    icon: PiHouse,
   },
   {
     title: "Explore",
@@ -44,19 +44,25 @@ const Home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-lg w-full h-full flex relative">
         {/* left sidebar*/}
-        <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
-          {navigation_items.map((item) => (
-            <Link
-              className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-6 rounded-3xl py-2 px-6"
-              href={`/${item.title.toLowerCase}`}
-              key={item.title}
-            >
-              <div>
-                <item.icon />
-              </div>
-              {item.title !== "x" && <div>{item.title}</div>}
-            </Link>
-          ))}
+        <section className="fixed w-72 flex flex-col item-stretch h-screen space-y-4">
+          <div className="flex flex-col item-stretch h-screen space-y-4 mt-4">
+            {navigation_items.map((item) => (
+              <Link
+                className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-6 rounded-3xl py-2 px-6"
+                href={`/${item.title.toLowerCase}`}
+                key={item.title}
+              >
+                <div>
+                  <item.icon />
+                </div>
+                {item.title !== "x" && <div>{item.title}</div>}
+              </Link>
+            ))}
+            <button className="w-full rounded-3xl bg-white px-6 py-4 text-2xl text-center hover:bg-opacity-70 transition duration-200 text-black">
+              Post
+            </button>
+          </div>
+          <div> this is bottom</div>
         </section>
         <main></main>
         <section></section>
