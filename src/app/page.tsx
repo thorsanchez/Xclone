6ecf5624ec66086c583bc620a56ce969de32a5a1
thorsanchez <ths281@hi.is>
2025-11-13 15:27:04 +0000

    icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,41 @@
-import Image from "next/image";
+import { MdHomeFilled } from "react-icons/md";
+import { SlMagnifier } from "react-icons/sl";
+import { RiNotification2Line } from "react-icons/ri";
+import { CiMail } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
 
-export default function Home() {
+//https://react-icons.github.io/react-icons/
+const navigation_items = [{
+  title: 'Home',
+  icon:MdHomeFilled
+}, {
+  title:'Explore',
+  icon:SlMagnifier
+}, {
+  title:'Notification',
+  icon:RiNotification2Line
+}, {
+  title:'Messages',
+  icon:CiMail
+}, {
+  title:'Bookmark',
+  icon:CiBookmark
+}, {
+  title:'Profile',
+  icon:CiUser
+}]
+const Home = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-lg w-full h-full flex relative">
+      {/* left sidebar*/}
+        <section className="fixed w-72 flex flex-col">left sidebar</section>
+        <main>home</main>
+        <section>right sidebar</section>
+      </div>
     </div>
-  );
+  )
 }
+
+export default Home
